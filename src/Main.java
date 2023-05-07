91d85bf3c6a396d0b06be404fd66c571a61090c4
Nadezhda Krivorotova <chaus-fox@list.ru>
2023-05-07 16:00:00 +0600

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Amount = 16859; // стоимость билета
-        int Mile = 20; // стоимость мили
+        int amount = 16859; // стоимость билета
+        int mile = 20; // стоимость мили
 
         System.out.println("Начисленно миль:");
-        System.out.println(Amount / Mile);
+        System.out.println(amount / mile);
     }
 }
